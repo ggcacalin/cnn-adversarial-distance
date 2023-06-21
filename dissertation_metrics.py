2b5df11df0ca0f0cbc932d31316c92e81cad36a9
Cata Gheorghiu <117468209+ggcacalin@users.noreply.github.com>
2023-06-21 22:32:59 +0100

    Metrics v1.1
    L0 changed to have a tolerance and scale 0-1, and Frechet package changed to use iterative instead of recursive implementation.

--- dissertation_metrics.py
+++ dissertation_metrics.py
@@ -1,9 +1,9 @@
 import numpy as np
 from fastdtw import fastdtw
-from frechetdist import frdist
+from similaritymeasures import frechet_dist
 
 def L0(original, adversary):
-    return np.sum((original - adversary != 0))
+    return np.sum((np.abs(original - adversary) > 0.01)) / len(original)
 
 def L1(original, adversary):
     return np.sum(np.abs(original - adversary))
@@ -17,14 +17,14 @@ def Linf(original, adversary):
 def cosine(original, adversary):
     norm_1 = np.linalg.norm(original)
     norm_2 = np.linalg.norm(adversary)
-    return 1 - np.dot(original, adversary)/(norm_1 * norm_2)
+    return 1 - abs(np.dot(original, adversary)/(norm_1 * norm_2))
 
 def pearson(original, adversary):
     corr_matrix = np.corrcoef(original, adversary)
     return 1 - abs(corr_matrix[0][1])
 
 def DTW(original, adversary):
-    distance, _ = fastdtw(original, adversary, radius = len(original), dist=2)
+    distance, _ = fastdtw(original, adversary, radius = len(original), dist = 2)
     return distance
 
 def frechet(original, adversary):
@@ -36,4 +36,4 @@ def frechet(original, adversary):
     for element in adversary:
         adversary_3d.append([element, 0])
     adversary_3d = np.array(adversary_3d)
-    return frdist(original_3d, adversary_3d)
+    return frechet_dist(original_3d, adversary_3d)
