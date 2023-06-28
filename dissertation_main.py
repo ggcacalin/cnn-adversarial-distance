b3f4f44b35d259bdd8af649ae56f687dcab89df7
Cata Gheorghiu <117468209+ggcacalin@users.noreply.github.com>
2023-06-28 21:41:25 +0300

    RPS v2.0
    Added RPS as an additional, gradient-free algorithm to study the workings of different metrics with

--- dissertation_main.py
+++ dissertation_main.py
@@ -1,8 +1,10 @@
+import math
 import numpy as np
 import wfdb
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import random
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import normalize
@@ -15,7 +17,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, regularizers
 from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
-from keras.layers import Conv1D, MaxPooling1D
+from keras.layers import Conv1D, MaxPooling1D, GlobalAveragePooling1D
 
 plt.rc('font', size=18)
 plt.rcParams['figure.constrained_layout.use'] = True
@@ -69,7 +71,7 @@ def demo_plots(x_df, counts):
     for series in demo_series:
         ax.plot(range(len(series)), series, color = colors[color_index], label = str(color_index + 1))
         color_index += 1
-    ax.legend(loc='upper left', title='Classes', bbox_to_anchor=(1, 1.02))
+    ax.legend(loc = 'upper left', title = 'Classes', bbox_to_anchor = (1, 1.02))
     plt.show()
 
 #Plotting class overview
@@ -78,21 +80,20 @@ def demo_plots(x_df, counts):
 
 def trainCNN(x_np, y_np):
     #Getting train / test split
-    x_train, x_test, y_train, y_test = train_test_split(x_np, y_np, test_size = 0.2, stratify= y_np, random_state=68)
+    x_train, x_test, y_train, y_test = train_test_split(x_np, y_np, test_size = 0.2, stratify = y_np, random_state = 68)
     with tf.device('/device:GPU:1'):
-        epochs = 20
-        batch_size = 32
+        epochs = 500
+        batch_size = 20
         num_classes = 6
 
         # Normalize training data
         x_train = normalize(x_train)
         x_test = normalize(x_test)
-
         # Convert class vectors to binary class matrices
         y_train = keras.utils.to_categorical(y_train - 1, num_classes)
         y_test = keras.utils.to_categorical(y_test - 1, num_classes)
 
-        use_saved_model = False
+        use_saved_model = True
         if use_saved_model:
             model = keras.models.load_model("ECG.model")
         else:
@@ -179,7 +180,7 @@ def PGD_generate_simple(model, x_original, y_categ, iterations = 5, epsilon = 10
     #Return perturbed input
     return np.array(x_adversarial)
 
-def golden_search(model, metric, a, b, tol):
+def golden_search(metric, a, b, tol):
     gr = (math.sqrt(5) + 1) / 2
     original = tf.identity(a)
     c = tf.identity(b - (b - a) / gr)
@@ -202,7 +203,7 @@ def PGD_generate_golden(model, x_original, y_categ, iterations = 5, epsilon = 10
         adversary += tf.random.uniform(shape = [len(x_original[i])], minval = -epsilon,
                                        maxval = epsilon, dtype=tf.dtypes.float64)
         #Golden line search to clip
-        adversary = golden_search(model, metric, x_original[i], adversary, tol)
+        adversary = golden_search(metric, x_original[i], adversary, tol)
         print('-----------------')
 
         for iter in range(iterations):
@@ -214,16 +215,114 @@ def PGD_generate_golden(model, x_original, y_categ, iterations = 5, epsilon = 10
                 gradient = g.gradient(loss, var_adv)
             #Perturb and clip
             adversary += alpha * tf.sign(gradient[0])
-            adversary = golden_search(model, metric, x_original[i], adversary, tol)
+            adversary = golden_search(metric, x_original[i], adversary, tol)
         #Retain completed adversary
         x_adversarial.append(adversary)
         print(metric(x_original[i], adversary))
     #Return perturbed input
     return np.array(x_adversarial)
 
+def RPS_generate(model, x_original, y_categ, iterations = 5, N = 5, M = 2, epsilon = 1, beta = 0.8, metric = L2):
+    x_adversarial = []
+    stored_epsilon = epsilon
+    max_attempts = N ** 2
+    for i in range(len(y_categ)):
+        print(i)
+        #Initial neighbour generation - only keep if misclassifies
+        sample = []
+        sample_metrics = []
+        correct_prediction = np.argmax(y_categ[i])
+        for _ in range(N):
+            attempts = 1
+            #Randomly generate positions to be perturbed
+            perturbed_positions = random.sample(range(len(y_categ)), int(len(y_categ) * attempts / max_attempts))
+            adversary = np.copy(x_original[i])
+            for pos in perturbed_positions:
+                adversary[pos] += tf.random.uniform(shape=[1], minval = -epsilon,
+                                       maxval = epsilon, dtype=tf.dtypes.float64)
+
+            #adversary = x_original[i] + tf.random.uniform(shape = [len(x_original[i])], minval = -epsilon,
+            #                           maxval = epsilon, dtype=tf.dtypes.float64)
+            adversary_class = np.argmax(model(tf.expand_dims(adversary, axis=0), training = False), axis = 1)[0]
+            success_flag = False
+
+            while (not success_flag and attempts < max_attempts):
+                if adversary_class != correct_prediction:
+                    #Keep adversary and get its distance if effective
+                    sample.append(adversary)
+                    sample_metrics.append(metric(x_original[i], adversary))
+                    success_flag = True
+                else:
+                    #Generate new adversary if not effective
+                    attempts += 1
+                    perturbed_positions = random.sample(range(len(y_categ)),
+                                                        int(len(y_categ) * attempts / max_attempts))
+                    adversary = np.copy(x_original[i])
+                    for pos in perturbed_positions:
+                        adversary[pos] += tf.random.uniform(shape=[1], minval=-epsilon,
+                                                            maxval=epsilon, dtype=tf.dtypes.float64)
+                    adversary_class = np.argmax(model(tf.expand_dims(adversary, axis=0), training=False), axis=1)[0]
+
+            #Failsafe
+            if not success_flag:
+                continue
+        if len(sample_metrics) < 1:
+            x_adversarial.append(x_original[i])
+            continue
+        # Sort neighbours by distance and keep the best M
+        zipped = sorted(zip(sample_metrics, sample))
+        zipped = zipped[:M]
+        #Expand on neighbours in a shrinking window around the best points for a number of iterations
+        for _ in range(iterations):
+            best_tuple = list(zipped[0])
+            #print(best_tuple[0])
+            sample = []
+            sample_metrics = []
+            for element in zipped:
+                previous_adversary = list(element)[1]
+                #Generate N effective neighbours
+                for _ in range(N):
+                    attempts = 1
+                    adversary = previous_adversary + tf.random.uniform(shape=[len(previous_adversary)],
+                                                                       minval=-epsilon/20,
+                                                                       maxval=epsilon/20, dtype=tf.dtypes.float64)
+                    adversary_class = np.argmax(model(tf.expand_dims(adversary, axis=0), training=False), axis=1)[0]
+                    success_flag = False
+                    while (not success_flag and attempts < max_attempts):
+                        if adversary_class != correct_prediction:
+                            # Keep adversary and get its distance if effective
+                            sample.append(adversary)
+                            sample_metrics.append(metric(x_original[i], adversary))
+                            success_flag = True
+                        else:
+                            # Generate new adversary if not effective
+                            attempts += 1
+                            adversary = previous_adversary + tf.random.uniform(shape=[len(previous_adversary)],
+                                                                               minval=-epsilon/20,
+                                                                               maxval=epsilon/20, dtype=tf.dtypes.float64)
+                            adversary_class = np.argmax(model(tf.expand_dims(adversary, axis=0), training=False), axis=1)[0]
+            #Failsafe
+            if len(sample_metrics) < 1:
+                continue
+            # Sort neighbours by distance and keep the best M of the N*M posibilities
+            new_zipped = sorted(zip(sample_metrics, sample))
+            new_zipped = new_zipped[:M]
+            # If no improvements at all, just resample; otherwise update best points and use tighter window
+            if list(new_zipped[0])[0] < best_tuple[0]:
+                zipped = new_zipped.copy()
+                epsilon = beta * epsilon
+        #Retain completed adversary and reset bracket
+        adversary = tf.identity(list(zipped[0])[1])
+        x_adversarial.append(adversary)
+        epsilon = stored_epsilon
+        print(metric(x_original[i], adversary))
+    # Return perturbed input
+    return np.array(x_adversarial)
+
 model, _, x_test, y_test = trainCNN(x_np, y_np)
 #x_adv = PGD_generate_simple(model, x_test, y_test, epsilon=0.1, alpha=0.1, metric = frechet)
-x_adv = PGD_generate_golden(model, x_test, y_test, epsilon = 5, alpha=0.5, tol = 0.1, metric = L2)
+#x_adv = PGD_generate_golden(model, x_test, y_test, epsilon = 5, alpha=0.5, tol = 0.1, metric = L2)
+x_adv = RPS_generate(model, x_test, y_test, iterations=50, N=7, M=2, epsilon = 0.5, beta = 0.8)
 x_adv = normalize(x_adv)
 model.evaluate(x_test, y_test, verbose = 1)
 model.evaluate(x_adv, y_test, verbose = 1)
